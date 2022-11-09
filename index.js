8839a32259cc800d380b6a3179138c338c562e2b
Mattitsu <mattitsustreams@gmail.com>
2022-11-09 17:21:29 +0000

    Removed GoogleSheets connections

--- index.js
+++ index.js
@@ -5,32 +5,7 @@ require('dotenv').config() // remove this line if you are using replit
 
 // const keyFile = require("./credentials.json")
 
-// GOOGLE SHEETS STUFF
-const { google } = require('googleapis');
-const keys = require('./keys.json');
 
-const auth = new google.auth.JWT(
-		keys.client_email, 
-		null, 
-		keys.private_key, 
-		['https://www.googleapis.com/auth/spreadsheets']
-);
-
-auth.authorize(function(err, tokens){
-	if(err){
-		console.log(err);
-		return;
-	} else {
-		console.log("Connected")
-		
-	}
-});
-
-const gsapi = google.sheets({version: "v4", auth: auth });
-
-
-
-// SHEETS STUFF ENDS
 
 const client = new Client({
 	intents: [
@@ -44,8 +19,7 @@ const client = new Client({
 	partials: [Partials.Channel, Partials.Message, Partials.User, Partials.GuildMember, Partials.Reaction] 
 });
 
-client.auth = auth
-client.sheets = gsapi.spreadsheets
+
 client.commands = new Collection()
 client.aliases = new Collection()
 client.slashCommands = new Collection();
